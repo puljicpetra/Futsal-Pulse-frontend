7f6c21e870d1be0708538804e088b3ddb89defeb
Petra Puljić <ppuljicc@gmail.com>
2025-02-06 18:48:06 +0100

    BACKEND_BASE_URL

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,29 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from 'vue';
+
+const message = ref('');
+
+onMounted(async () => {
+  try {
+    const res = await fetch(import.meta.env.VITE_BACKEND_BASE_URL + '/');
+    if (res.ok) {
+      message.value = 'Backend radi!';
+    } else {
+      message.value = 'Greška!';
+    }
+  } catch (error) {
+    console.error('Greška:', error);
+    message.value = 'Greška!';
+  }
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <p style="text-align: center; font-size: 1.2em; color: #333; margin-top: 20px;">{{ message }}</p>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+p {
+  font-weight: bold;
 }
 </style>
