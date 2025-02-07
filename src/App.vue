1d3757f41515138af5ec75c726137c1d1659762a
Petra Puljić <ppuljicc@gmail.com>
2025-02-07 14:50:33 +0100

    Home.vue

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,20 @@
 </script>
 
 <template>
-  <div class="fullscreen-background">
-    <img src="/src/assets/FutsalHome.jpg" alt="Futsal Home" class="fullscreen-image" />
+  <div id="app">
+    <router-view />
   </div>
 </template>
 
 <style scoped>
-.fullscreen-background {
-  position: fixed;
-  top: 0;
-  left: 0;
+#app {
   width: 100vw;
   height: 100vh;
-  z-index: -1;
-}
-
-.fullscreen-image {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
+  margin: 0;
+  padding: 0;
   opacity: 0.4;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="home"></div>
+  </template>
+  
+  <style scoped>
+  .home {
+    width: 100vw;
+    height: 100vh;
+    position: relative;
+    background: url('@/assets/FutsalHome.jpg') no-repeat center center;
+    background-size: cover;
+    background-attachment: fixed;
+  }
+  </style>
+  
