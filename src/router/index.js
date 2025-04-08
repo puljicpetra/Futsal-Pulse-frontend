31ba6a67b1698359b97234675cf4a36fae88fa84
Petra Puljić <ppuljicc@gmail.com>
2025-04-08 12:11:26 +0200

    Add initial views for Tournaments, Teams, and Matches
    [plugin:vite:vue] At least one <template> or <script> is required in a single file component.

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
+import Tournaments from '@/views/Tournaments.vue';
+import Matches from '@/views/Matches.vue';
+import Teams from '@/views/Teams.vue';
 
 const routes = [
   {
@@ -11,6 +14,21 @@ const routes = [
     path: '/home',
     name: 'HomeRoute',
     component: Home,
+  },
+  { 
+    path: '/tournaments', 
+    name: 'Tournaments', 
+    component: Tournaments 
+  },
+  { 
+    path: '/matches', 
+    name: 'Matches', 
+    component: Matches 
+  },
+  { 
+    path: '/teams', 
+    name: 'Teams', 
+    component: Teams 
   },
 ];
 
