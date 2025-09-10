ce9375bfa09644082de29b93c82f8e33e79c318f
Petra Puljić <ppuljicc@gmail.com>
2025-09-10 13:17:03 +0200

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 
-const apiBase = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3001'
+const apiBase =
+    import.meta.env.VITE_API_BASE_URL ||
+    import.meta.env.VITE_BACKEND_BASE_URL ||
+    'http://localhost:3001'
 
 let isHandlingAuthError = false
 
